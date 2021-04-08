ae913b6036b9956205bda6fa3b93c0ed32b67e7a
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-04-08 07:40:24 -0400

    Use latest input, add qqnorm plots

--- scripts/plot.R
+++ scripts/plot.R
@@ -21,3 +21,16 @@ ggplot(dd, aes(source, flux/24, fill = source)) +
   labs(x = 'Source', y = 'Flux (g/m2-h)') +
   theme(legend.position = 'none')
 ggsave('../plots/flux_box.png', height = 4, width = 6)
+
+ggplot(dl, aes(sample = tcn, colour = tc)) +
+  geom_qq() +
+  stat_qq_line() +
+  facet_wrap(~ interaction(stor.type1, source), scale = 'free') 
+ggsave('../plots/qqnorm.png', height = 4, width = 6)
+
+ggplot(dl, aes(sample = log10(tcn), colour = tc)) +
+  geom_qq() +
+  stat_qq_line() +
+  facet_wrap(~ interaction(stor.type1, source), scale = 'free') 
+ggsave('../plots/qqlognorm.png', height = 4, width = 6)
+
